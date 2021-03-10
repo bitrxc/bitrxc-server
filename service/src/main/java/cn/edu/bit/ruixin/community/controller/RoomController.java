458df1142b5f04921191aea9d33893ec9fbd6365
WuS-bit <wus2020@aliyun.com>
2021-03-10 15:58:36 +0800

    [feat] Modify Manager interface and Appointment interface

--- service/src/main/java/cn/edu/bit/ruixin/community/controller/RoomController.java
+++ service/src/main/java/cn/edu/bit/ruixin/community/controller/RoomController.java
@@ -80,7 +80,7 @@ public class RoomController {
         map.put("totalPages", page.getTotalPages());
         map.put("hasNext", page.hasNext());
         map.put("hasPrevious", page.hasPrevious());
-        map.put("items", infoVos);
+        map.put("rooms", infoVos);
         return CommonResult.ok(ResultCode.SUCCESS).data(map);
     }
 
