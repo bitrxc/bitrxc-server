e4b018804a86082890b2d8f460179a5161a1d5e5
WuS-bit <wus2020@aliyun.com>
2021-03-28 19:30:14 +0800

    [feat] Add Session Manager and Authentication interface

--- service/src/main/java/cn/edu/bit/ruixin/community/repository/RoomsRepository.java
+++ service/src/main/java/cn/edu/bit/ruixin/community/repository/RoomsRepository.java
@@ -18,5 +18,5 @@ import java.util.List;
 public interface RoomsRepository extends JpaRepository<Room, Integer>, JpaSpecificationExecutor<Room> {
     Room findRoomByName(String name);
     List<Room> findAllByNameLike(String name);
-    void addOne(Room room);
+//    void addOne(Room room);
 }
