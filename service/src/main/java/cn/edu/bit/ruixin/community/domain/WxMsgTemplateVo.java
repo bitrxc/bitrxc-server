75f00130e4f27d67c730a33a4345e2a225e01589
jingkaimori <wuyiheng6@163.com>
2021-09-09 12:27:13 +0800

    [fix & chore] fix incorrect date, new interface
    试用新日志接口

--- service/src/main/java/cn/edu/bit/ruixin/community/domain/WxMsgTemplateVo.java
+++ service/src/main/java/cn/edu/bit/ruixin/community/domain/WxMsgTemplateVo.java
@@ -1,5 +1,6 @@
 package cn.edu.bit.ruixin.community.domain;
 
+import java.text.SimpleDateFormat;
 import java.util.Date;
 
 import lombok.Data;
@@ -7,7 +8,7 @@ import lombok.NoArgsConstructor;
 
 /**
  * 微信订阅消息的模板参数，模板id{@code F5-LCFCIXt04AY0WPSC0vJAQsjyFXOn2vltNKXs5ABQ}
- * 由于列表格式复杂，故手工编写setter
+ * 由于列表格式复杂，而且date转为字符串的格式特殊，故手工编写setter
  *
  * @author jingkaimori
  * @date 2021/8/24
@@ -17,8 +18,8 @@ public class WxMsgTemplateVo {
     public WxMsgTemplateVo(){
         this.name5 = new Wrapper<String>();
         this.thing2 = new Wrapper<String>();
-        this.date3 = new Wrapper<Date>();
-        this.date4 = new Wrapper<Date>();
+        this.date3 = new Wrapper<String>();
+        this.date4 = new Wrapper<String>();
         this.phrase8 = new Wrapper<String>();
     }
 
@@ -32,15 +33,19 @@ public class WxMsgTemplateVo {
         this.phrase8.setValue(arg);
     }
     public void setDate3(Date arg) {
-        this.date3.setValue(arg);
+        SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd");
+        String date = dateFormat.format(arg);
+        this.date3.setValue(date);
     }
     public void setDate4(Date arg) {
-        this.date4.setValue(arg);
+        SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd");
+        String date = dateFormat.format(arg);
+        this.date4.setValue(date);
     }
     private Wrapper<String> name5;
     private Wrapper<String> thing2;
-    private Wrapper<Date> date3;
-    private Wrapper<Date> date4;
+    private Wrapper<String> date3;
+    private Wrapper<String> date4;
     private Wrapper<String> phrase8;
 }
 
