c1d7a6fb5eae82e787bcd420c4358648f4a5b41c
blacknick <blacknick123@163.com>
2021-10-07 00:30:56 +0800

    [feat] add get available period by room_id and get all time for admin

--- service/src/main/java/cn/edu/bit/ruixin/community/filter/TokenBasicFilter.java
+++ service/src/main/java/cn/edu/bit/ruixin/community/filter/TokenBasicFilter.java
@@ -11,6 +11,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.web.authentication.www.BasicAuthenticationFilter;
 import org.springframework.web.filter.OncePerRequestFilter;
 
 import javax.servlet.FilterChain;
