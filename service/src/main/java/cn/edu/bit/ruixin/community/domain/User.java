dec41b32805c184039de389fff5bf513aa039abe
jingkaimori <jingkaimori@gmail.com>
2022-03-30 20:58:09 +0800

    [refactor] remove dead code

--- service/src/main/java/cn/edu/bit/ruixin/community/domain/User.java
+++ service/src/main/java/cn/edu/bit/ruixin/community/domain/User.java
@@ -1,11 +1,6 @@
 package cn.edu.bit.ruixin.community.domain;
 
 import lombok.Data;
-//import org.springframework.security.core.GrantedAuthority;
-//import org.springframework.security.core.authority.SimpleGrantedAuthority;
-//import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.util.StringUtils;
-
 import javax.persistence.*;
 import java.util.ArrayList;
 import java.util.List;
@@ -27,6 +22,7 @@ public class User {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "id")
     private Integer id;
+    /** 用户的微信openid */
     @Column(name = "wxid")
     private String username;
     @Column(name = "name")
@@ -43,39 +39,4 @@ public class User {
     /** 用户资质是否经过核验 */
     @Column(name = "checked")
     private Boolean checked;
-    @Transient
-    private List<String> permissionValueList = new ArrayList<>();
-
-//    @Override
-//    public Collection<? extends GrantedAuthority> getAuthorities() {
-//        Collection<GrantedAuthority> authorities = new ArrayList<GrantedAuthority>();
-//        for (String permission:permissionValueList
-//        ) {
-//            if (!StringUtils.isEmpty(permission)) {
-//                SimpleGrantedAuthority authority = new SimpleGrantedAuthority(permission);
-//                authorities.add(authority);
-//            }
-//        }
-//        return authorities;
-//    }
-//
-//    @Override
-//    public boolean isAccountNonExpired() {
-//        return true;
-//    }
-//
-//    @Override
-//    public boolean isAccountNonLocked() {
-//        return true;
-//    }
-//
-//    @Override
-//    public boolean isCredentialsNonExpired() {
-//        return true;
-//    }
-//
-//    @Override
-//    public boolean isEnabled() {
-//        return true;
-//    }
 }
